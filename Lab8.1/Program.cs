099e1260a9021930dd4ddb066b0458dd76370dd1
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-15 01:48:57 +0300

    добавил решение 8.1

--- Lab8.1/Program.cs
+++ Lab8.1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab8._1
 {
@@ -10,6 +11,67 @@ namespace Lab8._1
     {
         static void Main(string[] args)
         {
+            #region
+            //DriveInfo[] drives = DriveInfo.GetDrives();
+            //foreach (DriveInfo d in drives)
+            //{
+            //    Console.WriteLine("Название диска {0}", d.Name);
+            //    if (d.IsReady)
+            //    {
+            //        Console.WriteLine("Объём диска {0}", d.TotalSize);
+            //        Console.WriteLine("Объём свободного пространства {0}", d.AvailableFreeSpace);
+            //        Console.WriteLine();
+            //    }
+            //    Console.ReadKey();
+            //}
+            //string path = "Logs2/Log.txt";
+            //if (!Directory.Exists(path))
+            //{ 
+            //    Directory.CreateDirectory(path);    
+            //}
+            //DirectoryInfo dir = new DirectoryInfo(path);
+            //{
+            //    if (!dir.Exists) 
+            //    {
+            //        dir.Create();
+            //    }
+            //}
+            //if (!File.Exists(path)) 
+            //{
+            //    File.Create(path);
+            //}
+            //using (StreamWriter sw = new StreamWriter(path, true))
+            //{
+            //    sw.WriteLine("Старт программы");
+            //    sw.WriteLine("Ошибка программы");
+            //}
+            //using (StreamReader sr = new StreamReader(path))
+            //{
+            //    Console.WriteLine(sr.ReadToEnd());
+            //}
+            //Console.ReadLine();
+            //Console.ReadKey();
+            #endregion
+            string dir = "C:/Users/R/Desktop/Автоматизация BIM проектирования/Lab8";
+            string[] path = Directory.GetDirectories(dir);
+            string[] files = Directory.GetFiles(dir);
+
+            for (int i = 0; i < path.Length; i++)
+            {
+                Console.WriteLine(path[i]);
+                foreach (string f in Directory.GetFileSystemEntries(path[i]))
+                {
+                    Console.WriteLine(f);
+                }
+            }
+            for (int j = 0; j < files.Length; j++)
+            {
+                Console.WriteLine(files[j]);
+            }
+
+            Console.ReadKey();
+
+
         }
     }
 }
